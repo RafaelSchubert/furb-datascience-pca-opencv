5d4e446f19cd8a993a98ea47317414a6e9290c3c
Rafael S. Campos <rafael.schubert.campos@gmail.com>
2021-01-13 23:47:04 -0300

    Data set loading's skeleton.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,4 +1,38 @@
+#include <string>
+#include <vector>
+
+#include <opencv2/core/mat.hpp>
+
+#include "FaceImage.h"
+
+
+static constexpr char  DATA_SET_PATH[] = "";
+static constexpr float TRAIN_SET_RATIO = .7f;
+
+
+std::vector<FaceImage> loadDataSet(std::string_view const& datasetDirectoryPath)
+{
+    return {};
+}
+
+
+std::pair<
+        std::vector<FaceImage*>,
+        std::vector<FaceImage*>
+    > splitDataSet(
+        std::vector<FaceImage> const& dataSet,
+        float const                   trainRatio
+    )
+{
+    return {};
+}
+
+
 int main()
 {
+    auto&& facesDataSet = loadDataSet(DATA_SET_PATH);
+
+    auto&& [train, test] = splitDataSet(facesDataSet, TRAIN_SET_RATIO);
+
     return 0;
 }
